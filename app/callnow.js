4aa094983145c4adc997ecfa3a08826b362ebf46
yogevzipori <yogevzipori@gmail.com>
2023-02-20 13:56:26 -0800

    phone#

--- app/callnow.js
+++ app/callnow.js
@@ -4,7 +4,7 @@ export default function callNow() {
   return (
     <div className="mt-10 mb-4 flex items-center justify-center gap-x-6">
       <Link
-        href="tel:7023346800"
+        href="tel:7026253387"
         style={{ textDecoration: "underline", color: "white" }}
         className="rounded-md bg-indigo-800 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
       >
