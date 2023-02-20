d11e90ae86385998fda67b9d9be3ea5f7c29e378
yogevzipori <yogevzipori@gmail.com>
2023-02-20 14:12:03 -0800

    fix1

--- app/banner.js
+++ app/banner.js
@@ -3,8 +3,8 @@ import { XMarkIcon } from '@heroicons/react/20/solid'
 export default function banner() {
   return (
 
-      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
-        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-indigo-800 py-2.5 px-6 sm:rounded-xl sm:py-3 sm:pr-3.5 sm:pl-4">
+      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-2 lg:px-8">
+        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-indigo-800 py-6.5 px-6 sm:rounded-xl sm:py-3 sm:pr-3.5 sm:pl-4">
           <p className="text-sm leading-6 text-white">
             <a href="tel:7026253387">
               <strong className="font-semibold">Don't wait!</strong>
--- app/head.js
+++ app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Zohan Handyman Services</title>
+      <title>Zohan Handyman Services - Las Vegas and Henderson</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="Handyman Services" content="Las Vegas Best Handyman Service" />
       {/* <link rel="icon" href="/favicon.ico" /> */}
