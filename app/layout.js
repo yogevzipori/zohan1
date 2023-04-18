ab60c4640f1290340625df7c79033ef3092bd0db
yogevzipori <yogevzipori@gmail.com>
2023-04-18 14:08:07 -0700

    navi

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,11 @@ import Mainnavi from './mainnavi'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Mainnavi />
       <head />
-      <body>{children}</body>
+      <body>
+      <Mainnavi />
+        {children}
+        </body>
     </html>
   )
 }
